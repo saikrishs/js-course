834715b1f5504c90ef9f6bff1e65e24fdff78c54
saikrishs <sai.srungavarapu@gmail.com>
2025-03-11 16:23:13 -0500

    created js-course project

---
+++ basic.js
@@ -0,0 +1,168 @@
+// console.log("Hello sai");
+// let UserName = "sai";
+// console.log(UserName);
+// UserName = "Srungavarapu";
+// console.log(UserName);
+// const age = 28;
+// console.log(age);
+
+// const myName = "sai";
+// const Age = 28;
+// const Desimal = 98.5;
+// console.log(myName);
+
+// var firstName = "Sai";
+// var lastName = "Srungavarapu";
+// // var fullName = firstName +" "+ lastName;
+// var fullName2 = `${firstName} ${lastName} is my full name`;
+
+// console.log(fullName2.toUpperCase());
+
+// // Array
+// const friends = ["sai","vamsi","gopi"];
+// console.log(friends);
+// let firstperson = ['sais, 28, flase'];
+// console.log(firstperson);
+
+// friends.push('ram');
+// console.log(friends);
+
+// //Object
+// let person ={
+//     firstName: "sai",
+//     lastName: "srungavarparu",
+//     age: 28,
+//     hobbies: ["plaing", "reading"]
+// };
+// console.log(person);
+// console.log(person.hobbies[1]);
+
+// // Array of objects
+// // let person =[];
+// let contact =[
+//     {
+//     firstName: "sai",
+//     lastName: "srungavarparu",
+//     age: 28
+//     },
+//     {firstName: "sai",
+//     lastName: "srungavarparu",
+//     age: 28
+//     },
+//     {
+//         firstName: "sai",
+//         lastName: "srungavarparu",
+//         age: 28
+//     }
+// ];
+// console.log(contact);
+
+// if else statement
+// let age = 10;
+// if (age === 10) {
+//     console.log('value is 10');
+// }else{
+//     console.log('Valeue is not 10');
+// }
+// else if
+// let age = 12;
+// if (age === 10) {
+//     console.log('value is 10');
+// }else if (age === 12){
+//     console.log('value is 12');
+// }else {
+//     console.log('Value is not 10');
+// }
+// checks first condition, check second condition, at last java script code is wrong excutes else condition
+
+// || or, && and
+// let age = 12;
+// let x = 13;
+// if (age === 10 || age === 11) {
+//     console.log('value is 10/11');
+// }else if (age === 12 && x == 13){
+//     console.log('value is 12 and 13');
+// }else {
+//     console.log('Value is not 10');
+// }
+
+// Switch statement
+
+// loops
+// for loop
+
+// for(let i = 0; i < 10; i ++){
+//     console.log(i);
+// }
+// Contact example
+
+// let contact =[
+//     {
+//     firstName: "sai",
+//     lastName: "srungavarparu",
+//     age: 28
+//     },
+//     {firstName: "sai",
+//     lastName: "srungavarparu",
+//     age: 28
+//     },
+//     {
+//         firstName: "sai",
+//         lastName: "srungavarparu",
+//         age: 28
+//     }
+// ];   
+// for(let i = 0; i < contact.length; i ++){
+//     console.log(contact[i]);
+// }
+
+// while loop
+// let contact =[
+//         {
+//             firstName: "sai",
+//             lastName: "srungavarparu",
+//              age: 28
+//         },
+//         {
+//             firstName: "vamsi",
+//             lastName: "srungavarparu",
+//             age: 28
+//         },
+//         {
+//             firstName: "gopi",
+//             lastName: "srungavarparu",
+//             age: 28
+//         }
+//     ];
+// let a = 0;
+//         while(a < contact.length){
+//             console.log(contact[a].firstName);
+//             a++;
+//         }
+// Symbol Operators
+// >
+// <
+// >=
+// <=
+// !=
+// ===
+// ==
+// &&
+
+
+// functions
+// function fullname(){}
+// function fullname(){
+//     firstName = "sai";
+//     lastName = "srungavarapu";
+//     console.log(`${firstName} ${lastName} is my full name`);
+// }
+// fullname();
+
+// lets make smarter
+// function fullname(firstName, lastName){
+//     console.log(`${firstName} ${lastName} is my full name`);
+// }
+// fullname( "sai","srungavrapu");
+
+// Arrow function
